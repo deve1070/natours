0a22793e8ce7e3d674035bde9d77b628e65099be
deve1070 <dkassa253@gmail.com>
2025-09-05 13:04:36 +0300

    Recatored the Api features

--- controller/tourController.js
+++ controller/tourController.js
@@ -4,49 +4,19 @@ exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
   req.query.sort = '-ratingsAverage,price';
   req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
 };
 
 exports.getAllTours = async (req, res) => {
   try {
-    const queryObj = { ...req.query };
-    const excludeQuery = ['page', 'limit', 'sort', 'fields'];
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
-    excludeQuery.forEach((el) => delete queryObj[el]);
-
-    let queryStirg = JSON.stringify(queryObj);
-    queryStirg = queryStirg.replace(
-      /\b(gte|gt|lte|lt)\b/g,
-      (match) => `$${match}`,
-    );
-
-    let query = Tour.find(JSON.parse(queryStirg));
-
-    // Sorting the tours
-
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-    // Field limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exsit');
-    }
-    const tours = await query;
+    // const tours = await query;
 
     res.status(200).json({
       status: 'success',
